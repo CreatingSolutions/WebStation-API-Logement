6d8f546eb50630e9f17c5e0d071581a3f7bde6ee
De Baerdemaeker Mathieu <debaerdemaekem@b13p23.fil.univ-lille1.fr>
2018-10-01 14:52:45 +0200

    fix: coverage

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,7 +2,6 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
-@Exc
 @RestController
 @EnableAutoConfiguration
 public class Example {
